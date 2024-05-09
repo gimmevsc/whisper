b3ac33b012b8157de71c02c21879ffe77c0617b9
Bohdan <bodya190505@gmail.com>
2024-05-09 18:46:00 +0200

    register

--- whisper/register/views.py
+++ whisper/register/views.py
@@ -1,17 +1,57 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.utils import timezone
+
+from back.whisper.register.models import User
 
 def registerUser(request):
-    if request.GET:
-        print(request.GET)
-    username = request.GET.get('username', '')
-    password = request.GET.get('password', '')
-    response_data = {
-        'username': username,
-        'password': int(password)
-                     }
-    return JsonResponse(response_data)
+    
+    if request.method == 'GET':
+        
+        username = request.GET.get('username')
+        email_address = request.GET.get('email_address')
+        password = request.GET.get('password')
 
+        if username and email_address and password:
+            
+            if User.objects.filter(email=email_address).exists():
+                return JsonResponse(
+                    {
+                        'status': 'error',
+                        'message': 'Email address already exists',
+                        'type': 'email'
+                    })
+            
+            if User.objects.filter(username=username).exists():
+                return JsonResponse(
+                    {   'status': 'error',
+                        'message': 'Username already exists',
+                        'type': 'username'
+                    })
+                
+            user = User.objects.create_user(username=username, email=email_address, password=password, created_at=)
+
+            user.created_at = timezone.now()
+            user.updated_at = timezone.now()
+            
+            user.save()
+
+            return JsonResponse(
+                {   
+                    'status': 'success'
+                })
+        else:
+            return JsonResponse(
+                {
+                    'status': 'error'
+                })
+
+    else:
+        return JsonResponse(
+                {
+                    'status': 'error'
+                })
+    
 def categories(request, catid):
     return HttpResponse(f"<h1>Category {catid}</h1>")
 
