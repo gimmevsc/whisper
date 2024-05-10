411807d10df229f9cea1abdf8481bd0f960e6715
MishaMalinov <mishokmalinok123@gmail.com>
2024-05-10 15:41:09 +0200

    eye in edge

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -48,11 +48,13 @@ function Register(){
 
             <h1 className={style.title}>Get Started Now!</h1>
             <div className={style.create_text}>Create your account here</div>
+            <div className={style.inputs}>
             {
                 labels.map((n,i)=>
                     <Input key={i} style={style} index={i} c={[fields,setFields]} field={fields} pressed={pressed} name={n[0]} inputType={n[1]} focusHandler={()=>focusHandler(i)} onClickOutside={()=>onClickOutsideHandler(i)}/>
                 )
             }
+            </div>
             <div className={style.buttons}>
                 <button className={`${style.register_btn} ${isAnimation?style.button_above:""}`} onClick={registerHandler}>
                     Register
