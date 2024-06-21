705fd5c1d5f2558ad21f967624638780d0cba2d1
Bohdan <bodya190505@gmail.com>
2024-06-21 17:44:46 +0200

    fixed admin user

--- whisper/chat/models.py
+++ whisper/chat/models.py
@@ -1,16 +1,16 @@
 
 from django.db import models
-from django.conf import settings
-
+# from django.conf import settings
+from register.models import User
 # Create your models here.
 
-# class UserProfileModel(models.Model):
-#     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     name = models.CharField(blank=True, null=True, max_length=100)
-#     online_status = models.BooleanField(default=False)
+class UserProfileModel(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(blank=True, null=True, max_length=100)
+    online_status = models.BooleanField(default=False)
 
-#     def __str__(self) -> str:
-#         return self.user.username
+    def __str__(self) -> str:
+        return self.user.username
 
 
 class ChatModel(models.Model):
@@ -23,29 +23,29 @@ class ChatModel(models.Model):
         return self.message
     
     
-# class ChatNotification(models.Model):
-#     chat = models.ForeignKey(to=ChatModel, on_delete=models.CASCADE)
-#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     is_seen = models.BooleanField(default=False)
+class ChatNotification(models.Model):
+    chat = models.ForeignKey(ChatModel, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_seen = models.BooleanField(default=False)
 
-#     def __str__(self) -> str:
-#         return self.user.username
+    def __str__(self) -> str:
+        return self.user.username
 
 
-# class ChatGroup(models.Model):
-#     group_name = models.CharField(max_length=128, unique=True, blank=True)
-#     admin = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='groupchats', blank=True, null=True, on_delete=models.SET_NULL)
-#     members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='chat_groups', blank=True)
-#     is_private = models.BooleanField(default=False)
+class ChatGroup(models.Model):
+    group_name = models.CharField(max_length=128, unique=True, blank=True)
+    admin = models.ForeignKey(User, related_name='groupchats', blank=True, null=True, on_delete=models.SET_NULL)
+    members = models.ManyToManyField(User, related_name='chat_groups', blank=True)
+    is_private = models.BooleanField(default=False)
     
-#     def __str__(self):
-#         return self.group_name
+    def __str__(self):
+        return self.group_name
 
-# class GroupMessage(models.Model):
-#     group = models.ForeignKey(ChatGroup, related_name='chat_messages', on_delete=models.CASCADE)
-#     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     body = models.CharField(max_length=300, blank=True, null=True)
-#     created = models.DateTimeField(auto_now_add=True)
+class GroupMessage(models.Model):
+    group = models.ForeignKey(ChatGroup, related_name='chat_messages', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.CharField(max_length=300, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
     
-#     class Meta:
-#         ordering = ['-created']
+    class Meta:
+        ordering = ['-created']
--- whisper/chat/views.py
+++ whisper/chat/views.py
@@ -1,20 +1,17 @@
-from django.http import JsonResponse
-from django.shortcuts import render, get_object_or_404
-from chat.models import ChatModel
-from register.models import User  # Assuming User is in register.models
-from django.core.exceptions import PermissionDenied
-from django.views.decorators.csrf import csrf_exempt
 import json
 import jwt
 from django.conf import settings
 from django.http import JsonResponse
-
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
+from django.views.decorators.csrf import csrf_exempt
+from chat.models import ChatModel
+from register.models import User  # Assuming User is in register.models
+from base64 import b64encode
 
 @csrf_exempt
 def chatPage(request, room_name):
-    
     if request.method == 'POST':
-        
         try:    
             data = json.loads(request.body.decode('utf-8'))
         except json.JSONDecodeError:
@@ -31,76 +28,82 @@ def chatPage(request, room_name):
         sender_token = data.get('sender_token')
         
         try:
-        # Decode the JWT token to get the payload
+            # Decode the JWT token to get the payload
             payload = jwt.decode(sender_token, settings.SECRET_KEY, algorithms=['HS256'])
             user_id = payload['user_id']  # Assuming 'user_id' is the key in your JWT payload
             sender = get_object_or_404(User, user_id=user_id)
         except jwt.ExpiredSignatureError:
             # Handle expired token error
-            raise ValueError('Token expired')
+            return JsonResponse(
+                {
+                    'message': 'Token expired',
+                    'status': 'error',
+                    'type': 'token_expired'
+                }, status=401
+            )
         except jwt.InvalidTokenError:
             # Handle invalid token error
-            raise ValueError('Invalid token')
+            return JsonResponse(
+                {
+                    'message': 'Invalid token',
+                    'status': 'error',
+                    'type': 'invalid_token'
+                }, status=401
+            )
         except User.DoesNotExist:
             # Handle user not found error
-            raise ValueError('User not found')
+            return JsonResponse(
+                {
+                    'message': 'User not found',
+                    'status': 'error',
+                    'type': 'user_not_found'
+                }, status=404
+            )
         
         if sender.user_id < receiver_id:
             thread_name = f'{receiver_id}-{sender.user_id}'
         else:
             thread_name = f'{sender.user_id}-{receiver_id}'
-        
-            
 
         # Fetch messages from the database filtered by thread_name
         messages = ChatModel.objects.filter(thread_name=thread_name)
         
-        receiver = User.objects.get(user_id=receiver_id)
+        receiver = get_object_or_404(User, user_id=receiver_id)
         
         sender_req = sender.user_id
 
-        receiver_avatar = receiver.profile_picture.url if receiver.profile_picture else None
-        sender_avatar = sender.profile_picture.url if sender.profile_picture else None
+        def get_avatar_base64(user):
+            if user.profile_picture:
+                with open(user.profile_picture.path, "rb") as image_file:
+                    return b64encode(image_file.read()).decode('utf-8')
+            return None
+
+        receiver_avatar = get_avatar_base64(receiver)
+        sender_avatar = get_avatar_base64(sender)
         
         message_list = []
         
         for message in messages:
             sender_username = message.sender
-            sender_id = User.objects.get(username=sender_username)
-            receiver = User.objects.get(user_id=receiver_id)
+            sender_id = get_object_or_404(User, username=sender_username)
             message_list.append({
                 'sender': sender_id.user_id,
-                'username' : sender_username,
+                'username': sender_username,
                 'message': message.message,
                 'timestamp': message.timestamp.strftime('%Y-%m-%d %H:%M:%S')
             })
 
         return JsonResponse(
-                {
-                    'message': message_list,
-                    'sender_id' : sender_req,
-                    'receiver_avatar' : receiver_avatar,
-                    'sender_avatar' : sender_avatar
-                }, status=200
-            )
+            {
+                'message': message_list,
+                'sender_id': sender_req,
+                'receiver_avatar': receiver_avatar,
+                'sender_avatar': sender_avatar
+            }, status=200
+        )
         
-    return Response({'error': 'Invalid request method'}, status=400)
-        # print(sender)
-        # Retrieve chat messages between sender and receiver
-        # messages = ChatMessage.objects.filter(sender=sender, receiver_id=receiver_id).order_by('timestamp')
-        # serializer = ChatMessageSerializer(messages, many=True)
-
-        # # Retrieve sender's and receiver's profile pictures (if available)
-        # sender_profile = get_object_or_404(UserProfile, user=sender)
-        # receiver = get_object_or_404(User, id=receiver_id)
-        # receiver_profile = get_object_or_404(UserProfile, user=receiver)
-
-        # # Serialize profile picture URLs (or base64 encoded images) if needed
-        # sender_avatar_url = sender_profile.profile_picture.url if sender_profile.profile_picture else None
-        # receiver_avatar_url = receiver_profile.profile_picture.url if receiver_profile.profile_picture else None
-
-        # return Response({
-        #     'messages': serializer.data,
-        #     'sender_avatar_url': sender_avatar_url,
-        #     'receiver_avatar_url': receiver_avatar_url
-        # })
+    return JsonResponse(
+        {
+            'error': 'Invalid request method'
+        }, status=400
+    )
