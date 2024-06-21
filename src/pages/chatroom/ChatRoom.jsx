515cd6010582f5f98999074386b0b4de0b5862e7
MishaMalinov <mishokmalinok123@gmail.com>
2024-06-21 17:18:20 +0200

    hz

--- src/pages/chatroom/ChatRoom.jsx
+++ src/pages/chatroom/ChatRoom.jsx
@@ -4,42 +4,53 @@ import { useParams } from "react-router-dom";
 import Cookies from 'js-cookie';
 import config from '../../config.json';
 import ReconnectingWebSocket from 'reconnecting-websocket';
-
+import style from "./chatroom.module.scss"
 function ChatRoom() {
     const [messages, setMessages] = useState([]);
+    const [sender,setSender] = useState("");
+    const [receiver,setReceiver] = useState("");
+    const [loaded, setLoaded] = useState(false)
     const messageInput = useRef();
+    const [receiverImg,setReceiverImg] = useState(null);
     const {room} = useParams(); // Replace with dynamic room name as needed
+    const messagesRef = useRef(null)
     const ws = useRef(null);
     useEffect(() => {
-        console.log(room)
-        const url = `${config.url}/chat/${room}`
-
-        const data = {
-            "receiver":room,
-            "sender_token" : Cookies.get('token')
-        } 
-        axios.post(url,data).then(res=>{
-            console.log("Load the page")
-            // console.log(res.data)
-        }).catch(err=>console.log("what"))
+        
         ws.current =new ReconnectingWebSocket(`${config.url.replace(/^http/, 'ws')}/ws/chat/${room}`);
-        // Fetch initial chat messages
         
-
-               
-
-        // Set up WebSocket for real-time updates
-        
-
         ws.current.onopen = () => {
             console.log('WebSocket connection established');
-            
+            if (!loaded){
+                console.log(room)
+    
+                const url = `${config.url}/chat/${room}`
+    
+                const data = {
+                    "receiver":room,
+                    "sender_token" : Cookies.get('token')
+                } 
+                axios.post(url,data).then(res=>{
+                    
+                    setSender(res.data.sender_id)
+                    // setReceiver(res.data.receiver_id)
+                    setReceiverImg(res.data.receiver_avatar);
+                    setMessages(res.data.message)
+                    setLoaded(true)
+                }).then(()=>{
+                    
+                    scrollToBottom()
+
+                }).catch(err=>console.log(err))
+            }
         };
 
         ws.current.onmessage = (event) => {
             const data = JSON.parse(event.data);
-            console.log(event.data)
+            // console.log(event.data)
             setMessages(prevMessages => [...prevMessages, data]);
+            scrollToBottom()
+
         };
 
         ws.onclose = () => {
@@ -55,31 +66,47 @@ function ChatRoom() {
             ws.current.close();
         };
     }, []);
+    const scrollToBottom = ()=>{
+        setTimeout(() => {
+            messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+        }, 1);
+    }
     function sendMessageHandler(){
         console.log(messages)
         ws.current.send(messageInput.current.value)
+        messageInput.current.value = ""
     }
     return (
-        <div className="main">
+        <div className={style["main"]}>
             
-            <div className="chats">
+            <div className={style["chats"]}>
                 chats
             </div>
 
-            <div className="chat">
+            <div className={style["chat"]}>
                 <div className="top-bar">
                     Username
                 </div>
-                <div className="messages">
+                <div className={style["bot"]}>
+                <div className={style["messages"]} ref={messagesRef}>
                     {messages.map((msg, index) => (
-                        <div key={index}>
-                            <strong>{msg.username}:</strong> {msg.message}
+                        <div key={index} className={style.message}>
+                            <div className={style["message-data"]} style={msg.sender==sender ? {right:0}:{}}>
+                                {msg.sender!=sender && <img src={receiverImg} alt="avatar" />} {msg.message}
+                            </div>
+                            <div className={style["message-data"]} style={{opacity:0,position:"relative"}}>
+                                <strong>{msg.username}:</strong> {msg.message}
+                            </div>
+                            
                         </div>
                     ))}
                 </div>
-
-                <input type="text" ref={messageInput} onChange={() => console.log(messageInput.current.value)} />
-                <button onClick={sendMessageHandler}>Click</button>
+                <div className={style['input-box']}>
+                    <input className={style.input} type="text" ref={messageInput} onKeyDown={(e)=>{if(e.key =="Enter")sendMessageHandler()}} />
+                    <button className={style["send-btn"]} onClick={sendMessageHandler}>Send</button>
+                </div>
+                </div>
+                
             </div>
         </div>
     );
